8b833db22ae4a203c41ca63eb73fc9407068ebbe
AndreaZ2306 <117767655+AndreaZ2306@users.noreply.github.com>
2025-12-28 13:57:46 +0100

    [OKR2][#2.2] Import TestSecurityConfig in application tests

--- resource-server/src/test/java/com/aghairsalon/resourceserver/ResourceserverApplicationTests.java
+++ resource-server/src/test/java/com/aghairsalon/resourceserver/ResourceserverApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 class ResourceserverApplicationTests {
 
     @Test
